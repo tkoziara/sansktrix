ba5acc7357056a22e6efea69e3c578d403a309f9
Tomasz Koziara <tkoziara@gmail.com>
2025-03-20 18:50:43 +0100

    Added thread safety and smoothing + screenshot image

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sansktrix
 Sanskrit-Themed Matrix Screensaver for Your Linux/Unix Terminal – Watch Devanagari Characters Flow Like Digital Rain!
+
+![Sanskrit-Themed Matrix Screensaver](screenshot.png)
--- sansktrix.cpp
+++ sansktrix.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <random>
 #include <atomic>
+#include <mutex>
+#include <iostream>
 
 // Devanagari letters representing the Sanskrit alphabet
 std::vector<const char*> sanskrit_alphabet = {
@@ -19,11 +21,32 @@ std::vector<const char*> sanskrit_alphabet = {
     "ळ", "क्ष", "ज्ञ"
 };
 
+// First column to print
+int start_col = 1;
+
+// Column step
+int col_step = 4;
+
 // Screen size
 int max_row, max_col;
 
 // Atomic flag to control thread termination
 std::atomic<bool> terminate_thread(false);
+
+// Screen buffer access mutex
+std::mutex screen_mutex;
+
+// Clear vertical screen column
+void clear_column(int y) {
+    std::lock_guard<std::mutex> lock(screen_mutex);
+    for (int x = 0; x < max_row; ++x) {
+        for (int i = -1; i <= 1; ++i) {
+            if (y+i >= 0 && y+i < max_col) {
+                mvprintw(x, y+i, " "); // Overwrite with space
+            }
+        }
+    }
+}
 
 void thread_print(int y)
 {
@@ -31,35 +54,48 @@ void thread_print(int y)
     std::mt19937 gen(rd());  // Mersenne Twister engine, seeded by random_device
     std::uniform_int_distribution<> dist(50, 500);
     std::vector<const char*> letter(1);
-    thread_local int col_length = -1;
+    thread_local int x = 0;
 
     while (!terminate_thread)
     {
-        // Next row index
-        int x = col_length + 1;
-
         // Sample single character
         std::sample(sanskrit_alphabet.begin(), sanskrit_alphabet.end(), letter.begin(), 1, gen);
 
-        mvprintw(x, y, "%s", letter[0]);
+        // Print character
+        {
+            std::lock_guard<std::mutex> lock(screen_mutex);
+            mvprintw(x, y, "%s", letter[0]);
+        }
 
-        if (x > max_row) x = -1;
+        // Next row index
+        x = std::clamp(x + 1, -1, max_row-1);
 
-        col_length = x;
+        if (x == max_row-1) // Clear column if last row
+        {
+            x = 0;
+            clear_column(y);
+        }
 
         // Sleep between 50 and 500 ms
         std::this_thread::sleep_for(std::chrono::milliseconds(dist(gen)));
 
-        refresh();  // Refresh screen to show output
-
-        if (y == 0) // Check if a key has been pressed
+        // Refresh screen
         {
-            int ch = getch();
+            std::lock_guard<std::mutex> lock(screen_mutex);
+            refresh();
+        }
+    }
+}
 
-            if (ch != ERR) {  // A key has been pressed
-                terminate_thread = true;  // Set flag to terminate the thread
-                break;
-            }
+void press_any_key_thread()
+{
+    while (!terminate_thread)
+    {
+        int ch = getch();
+
+        if (ch != ERR) {  // A key has been pressed
+            terminate_thread = true;  // Set flag to terminate the thread
+            break;
         }
     }
 }
@@ -77,15 +113,17 @@ int main() {
     getmaxyx(stdscr, max_row, max_col);
 
     // Create printing threads
-    std::vector<std::thread> printing_threads;
+    std::vector<std::thread> threads;
 
-    for (int i = 0; i<max_col; i += 2)
+    for (int i = start_col; i<max_col; i += col_step)
     {
-        printing_threads.emplace_back(thread_print, i);
+        threads.emplace_back(thread_print, i);
     }
 
+    threads.emplace_back(press_any_key_thread);
+
     // Join all threads
-    for (auto& t : printing_threads)
+    for (auto& t : threads)
     {
         t.join();
     }
